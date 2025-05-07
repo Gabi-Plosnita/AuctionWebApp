cecaf41cd819620a7db6da97eb80d9aba48f17fc
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-07 20:57:47 +0300

    Create AuctionImageRequest.cs

---
+++ AuctionWebApp/AuctionWebApp/Models/AuctionImage/AuctionImageRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionWebApp.Models;
+
+public class AuctionImageRequest
+{
+	[Required(ErrorMessage = "Image data is required")]
+	[MinLength(1, ErrorMessage = "Image data cannot be empty")]
+	public byte[] ImageData { get; set; } = Array.Empty<byte>();
+
+	[Required(ErrorMessage = "Image content type is required")]
+	[RegularExpression(@"^[A-Za-z0-9\-\+\.]+\/[A-Za-z0-9\-\+\.]+$", ErrorMessage = "Invalid image content type format")]
+	public string ImageContentType { get; set; } = string.Empty;
+}
