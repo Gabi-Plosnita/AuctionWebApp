74ec38912531b9b1b4d8c02c3c157a062aa12956
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-07 21:29:56 +0300

    Create models for Category

---
+++ AuctionWebApp/AuctionWebApp/Models/Category/CreateCategoryRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionWebApp.Models;
+
+public class CreateCategoryRequest
+{
+	[Required(ErrorMessage = "Name is required")]
+	[StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters")]
+	public string Name { get; set; } = string.Empty;
+}
---
+++ AuctionWebApp/AuctionWebApp/Models/Category/UpdateCategoryRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuctionWebApp.Models;
+
+public class UpdateCategoryRequest
+{
+	[Required(ErrorMessage = "Name is required")]
+	[StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters")]
+	public string Name { get; init; } = string.Empty;
+}
