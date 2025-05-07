9921b74e6455956648e8f449559565ddcd938345
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-07 21:56:17 +0300

    Create Driver models

---
+++ AuctionWebApp/AuctionWebApp/Models/Driver/DriverResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace AuctionWebApp.Models;
+
+public record DriverResponse
+{
+	public int Id { get; init; }
+
+	public Role Role { get; init; }
+
+	public string Email { get; init; } = string.Empty;
+}
---
+++ AuctionWebApp/AuctionWebApp/Models/Driver/RegisterDriverRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AuctionWebApp.Models;
+
+public record RegisterDriverRequest
+{
+	public string Email { get; init; } = string.Empty;
+
+	public string Password { get; init; } = string.Empty;
+}
