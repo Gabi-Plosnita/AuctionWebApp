4c95d16a3c1c46ab0868e2448f8492063990caff
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-07 22:05:14 +0300

    Create User models

---
+++ AuctionWebApp/AuctionWebApp/Models/User/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace AuctionWebApp.Models;
+
+public record UserResponse
+{
+	public int Id { get; init; }
+
+	public Role Role { get; init; }
+
+	public string Email { get; init; } = string.Empty;
+
+	public string FirstName { get; init; } = string.Empty;
+
+	public string LastName { get; init; } = string.Empty;
+
+	public string Address { get; init; } = string.Empty;
+}
