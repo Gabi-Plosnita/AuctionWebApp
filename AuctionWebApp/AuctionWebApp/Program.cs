deeb4ba18745d3f8c0a6220b4b591aceae038895
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-08 15:22:25 +0300

    Create CookieHandler and add it in the HttpClient pipeline

--- AuctionWebApp/AuctionWebApp/Program.cs
+++ AuctionWebApp/AuctionWebApp/Program.cs
@@ -1,4 +1,5 @@
 using AuctionWebApp;
+using AuctionWebApp.HttpClients;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<CookieHandler>();
+
+builder.Services
+.AddHttpClient<IAuthHttpClient, AuthHttpClient>(client =>
+{
+	client.BaseAddress = new Uri("https://localhost:5001/");
+})
+.AddHttpMessageHandler<CookieHandler>();
+
 
 await builder.Build().RunAsync();
