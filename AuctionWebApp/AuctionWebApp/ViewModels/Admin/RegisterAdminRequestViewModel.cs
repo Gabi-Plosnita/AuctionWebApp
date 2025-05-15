47efe552387e853688e6e9d32ef9c66f69ff1e3b
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-15 14:13:51 +0300

    Update RegisterAdminRequestViewModel.cs

--- AuctionWebApp/AuctionWebApp/ViewModels/Admin/RegisterAdminRequestViewModel.cs
+++ AuctionWebApp/AuctionWebApp/ViewModels/Admin/RegisterAdminRequestViewModel.cs
@@ -6,9 +6,11 @@ public class RegisterAdminRequestViewModel
 {
 	[Required(ErrorMessage = "Email is required")]
 	[EmailAddress(ErrorMessage = "Invalid email address format")]
-	public required string Email { get; set; }
+	[DataType(DataType.EmailAddress)]
+	public string Email { get; set; } = string.Empty;
 
 	[Required(ErrorMessage = "Password is required")]
 	[StringLength(100, MinimumLength = 4, ErrorMessage = "Password must be between 4 and 100 characters")]
-	public required string Password { get; set; }
+	[DataType(DataType.Password)]
+	public string Password { get; set; } = string.Empty;
 }
