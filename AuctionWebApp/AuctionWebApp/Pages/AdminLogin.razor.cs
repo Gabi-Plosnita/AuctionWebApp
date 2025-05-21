2ae7f3f16043ee8f684417969b55c45c11384942
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-21 16:13:04 +0300

    Update login page

--- AuctionWebApp/AuctionWebApp/Pages/AdminLogin.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/AdminLogin.razor.cs
@@ -8,10 +8,30 @@ public partial class AdminLogin : ComponentBase
 {
 	[Inject]
 	public IAuthService _authService { get; set; } = default!;
+
+	[Inject]
+	public NavigationManager Navigation { get; set; } = default!;
+
 	protected LoginViewModel AdminModel { get; set; } = new LoginViewModel();
+	protected bool ShowPopup { get; set; }
+	protected List<string> ErrorMessages { get; set; } = new List<string>();
 
 	protected async Task HandleAdminLogin(LoginViewModel model)
 	{
 		var result = await _authService.LoginAdminAsync(model);
+		if (!result.HasErrors)
+		{
+			Navigation.NavigateTo("/");
+		}
+		else
+		{
+			ErrorMessages = result.Errors.ToList();
+			ShowPopup = true;
+		}
+	}
+
+	void ClosePopup()
+	{
+		ShowPopup = false;
 	}
 }
