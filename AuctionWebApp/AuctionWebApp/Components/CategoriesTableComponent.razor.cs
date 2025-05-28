1fe32a663801b2330ae39b91b4d15e1232a735c0
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-28 22:46:02 +0300

    Create new components

---
+++ AuctionWebApp/AuctionWebApp/Components/CategoriesTableComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class CategoriesComponent : ComponentBase
+{
+	[Inject]
+	public ICategoryService CategoryService { get; set; }
+
+	
+}
