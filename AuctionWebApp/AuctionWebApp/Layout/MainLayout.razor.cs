2ab596351dc622caf911f56b321a9b90d8f68cff
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-29 16:45:42 +0300

    Create MainLayout.razor.cs
    Each time we navigate to a page, the toasts are cleared

---
+++ AuctionWebApp/AuctionWebApp/Layout/MainLayout.razor.cs
@@ -0,0 +1,27 @@
+﻿namespace AuctionWebApp.Layout;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using MudBlazor;
+
+public partial class MainLayout : LayoutComponentBase, IDisposable
+{
+	[Inject] private ISnackbar Snackbar { get; set; } = default!;
+	[Inject] private NavigationManager Navigation { get; set; } = default!;
+
+	protected override void OnInitialized()
+	{
+		Navigation.LocationChanged += OnLocationChanged;
+	}
+
+	private void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+	{
+		Snackbar.Clear();
+	}
+
+	public void Dispose()
+	{
+		Navigation.LocationChanged -= OnLocationChanged;
+	}
+}
+
