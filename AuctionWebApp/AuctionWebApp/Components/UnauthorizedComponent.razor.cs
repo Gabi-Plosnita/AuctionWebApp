677783f1675559d4c380d4e5d9b417b31547428e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-29 14:53:32 +0300

    Create Unauthorized component and use it in App.razor

---
+++ AuctionWebApp/AuctionWebApp/Components/UnauthorizedComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class UnauthorizedComponent : ComponentBase
+{
+	[Inject]
+	protected NavigationManager NavigationManager { get; set; } = default!;
+
+	protected void GoHome()
+	{
+		NavigationManager.NavigateTo("/");
+	}
+}
