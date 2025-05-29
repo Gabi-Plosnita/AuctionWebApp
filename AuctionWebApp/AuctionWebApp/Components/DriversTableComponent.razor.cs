ec983182eeaafdd8e7fa876e9de24720e06b1f8f
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-30 00:16:26 +0300

    Create DriversTable component and use it in AdminDashboard

---
+++ AuctionWebApp/AuctionWebApp/Components/DriversTableComponent.razor.cs
@@ -0,0 +1,51 @@
+﻿using AuctionWebApp.Helpers;
+using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AuctionWebApp.Components;
+
+public partial class DriversTableComponent : ComponentBase
+{
+	[Inject]
+	private IDriverService DriverService { get; set; } = default!;
+
+	[Inject]
+	private ISnackbar Snackbar { get; set; } = default!;
+
+	[Inject]
+	protected NavigationManager NavigationManager { get; set; } = default!;
+
+
+	private List<DriverViewModel> drivers = new();
+
+	private bool _loading;
+
+	protected override async Task OnInitializedAsync()
+	{
+		_loading = true;
+		var result = await DriverService.GetAllAsync();
+		if (!result.HasErrors)
+			drivers = result.Data;
+		else
+		{
+			Snackbar.ShowErrors(result.Errors);
+		}
+		_loading = false;
+	}
+
+	private async Task DeleteDriverAsync(int id)
+	{
+		var result = await DriverService.DeleteByIdAsync(id);
+		if (result.HasErrors)
+		{
+			Snackbar.ShowErrors(result.Errors);
+		}
+		else
+		{
+			Snackbar.ShowSuccess("Driver deleted successfully.");
+			drivers.RemoveAll(driver => driver.Id == id);
+		}
+	}
+}
