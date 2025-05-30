6700c8c8a4203a47ac313636824815c2bf4ea674
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-30 22:37:45 +0300

    Update AssignDriver page

--- AuctionWebApp/AuctionWebApp/Pages/AssignDriverToAuction.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/AssignDriverToAuction.razor.cs
@@ -1,7 +1,55 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using AuctionWebApp.Helpers;
+using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace AuctionWebApp.Pages;
 
 public partial class AssignDriverToAuction : ComponentBase
 {
+	[Inject]
+	private IDriverService DriverService { get; set; } = default!;
+
+	[Inject]
+	private IAuctionService AuctionService { get; set; } = default!;
+
+	[Inject]
+	private ISnackbar Snackbar { get; set; } = default!;
+
+	[Parameter]
+	public int AuctionId { get; set; }
+
+	private DetailedAuctionViewModel auction;
+
+	private List<DriverViewModel> drivers = new List<DriverViewModel>();
+
+	protected override async Task OnInitializedAsync()
+	{
+		var auctionResult = await AuctionService.GetDetailedByIdAsync(AuctionId);
+		if (auctionResult.HasErrors)
+		{
+			Snackbar.ShowErrors(auctionResult.Errors);
+			return;
+		}
+		if (auctionResult.Data is null)
+		{
+			Snackbar.ShowError("Auction not found");
+			return;
+		}
+		auction = auctionResult.Data;
+
+		var driversResult = await DriverService.GetAllAsync();
+		if (driversResult.HasErrors)
+		{
+			Snackbar.ShowErrors(driversResult.Errors);
+			return;
+		}
+		if (driversResult.Data is null)
+		{
+			Snackbar.ShowError("No drivers found");
+			return;
+		}
+		drivers = driversResult.Data;
+	}
 }
