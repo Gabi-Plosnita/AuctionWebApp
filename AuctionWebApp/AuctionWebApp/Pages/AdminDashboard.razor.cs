37043c84aecb180f521328de584b771b58501687
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-30 11:38:08 +0300

    Update AdminDashboard page

--- AuctionWebApp/AuctionWebApp/Pages/AdminDashboard.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/AdminDashboard.razor.cs
@@ -1,7 +1,17 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using AuctionWebApp.Enums;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
 
 namespace AuctionWebApp.Pages;
 
 public partial class AdminDashboard : ComponentBase
 {
+	private AuctionFilterViewModel auctionsWithNoDriver = new AuctionFilterViewModel
+	{
+		Status = AuctionStatus.InTransit,
+		IgnoreDriverId = false,
+		DriverId = null,
+		PageSize = 10,
+		Page = 1
+	};
 }
