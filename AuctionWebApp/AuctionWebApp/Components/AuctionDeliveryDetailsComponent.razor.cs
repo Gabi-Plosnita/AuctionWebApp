46d709aa4e67933561f663cde7df7d768bbc660d
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-30 22:38:00 +0300

    Create new component for auction delivery details

--- AuctionWebApp/AuctionWebApp/Components/AuctionDeliveryDetailsComponent.razor.cs
+++ AuctionWebApp/AuctionWebApp/Components/AuctionDeliveryDetailsComponent.razor.cs
@@ -7,4 +7,16 @@ public partial class AuctionDeliveryDetailsComponent : ComponentBase
 {
 	[Parameter]
 	public required DetailedAuctionViewModel Auction { get; set; }
+
+	private UserViewModel? winner;
+
+	protected override async Task OnParametersSetAsync()
+	{
+		if (Auction?.Bids != null && Auction.Bids.Any())
+		{
+			winner = Auction.Bids
+				.OrderByDescending(b => b.Amount)
+				.FirstOrDefault()?.Bidder;
+		}
+	}
 }
