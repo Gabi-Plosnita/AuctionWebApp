1858ca904650852a2ed6e8179019497b33842c43
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-05-31 18:02:56 +0300

    Create and use Logout component

---
+++ AuctionWebApp/AuctionWebApp/Components/LogoutComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using AuctionWebApp.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class LogoutComponent : ComponentBase
+{
+	[Inject]
+	private NavigationManager NavigationManager { get; set; } = default!;
+
+	[Inject]
+
+	private IAuthService AuthService { get; set; } = default!;
+
+	private async Task LogoutAsync()
+	{
+		NavigationManager.NavigateTo("/");
+		await AuthService.LogoutAsync();
+	}
+}
--- AuctionWebApp/AuctionWebApp/Pages/AdminDashboard.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/AdminDashboard.razor.cs
@@ -1,11 +1,16 @@
 ﻿using AuctionWebApp.Enums;
 using AuctionWebApp.ViewModels;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
 
 namespace AuctionWebApp.Pages;
 
 public partial class AdminDashboard : ComponentBase
 {
+	[Inject]
+	private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
 	private AuctionFilterViewModel auctionsWithNoDriver = new AuctionFilterViewModel
 	{
 		Status = AuctionStatus.InTransit,
@@ -21,4 +26,17 @@ public partial class AdminDashboard : ComponentBase
 	private string? navigateUrl = "auctions/{id}/assign-driver";
 
 	private string? navigationButtonName = "View";
+
+	private bool isSuperAdmin;
+
+	private bool isLoading = true;	
+
+	protected override async Task OnInitializedAsync()
+	{
+		var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+		var user = authState.User;
+
+		isSuperAdmin = user.IsInRole("SuperAdmin");
+		isLoading = false;
+	}
 }
