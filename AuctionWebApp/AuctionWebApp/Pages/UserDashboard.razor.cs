5fa73b05dd6998572b265995397503146fd3b3f8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-01 17:42:28 +0300

    Add logic for MyBids tab

--- AuctionWebApp/AuctionWebApp/Pages/UserDashboard.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/UserDashboard.razor.cs
@@ -1,7 +1,37 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using AuctionWebApp.Enums;
+using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
 
 namespace AuctionWebApp.Pages;
 
 public partial class UserDashboard : ComponentBase
 {
+	[Inject]
+	private IAuthService AuthService { get; set; } = default!;
+
+	private string detailedAuctionUrl = "/auctions/{id}/bid";
+
+	private AuctionFilterViewModel myBidsAuctionFilter = new AuctionFilterViewModel
+	{
+		Status = AuctionStatus.InProgress,
+	};
+
+	private int authenticatedUserId;
+
+	protected override async Task OnInitializedAsync()
+	{
+		var result = await AuthService.GetAuthenticatedUserAsync();
+		if(result.HasErrors)
+		{
+			return;
+		}
+		if (result.Data == null)
+		{
+			return;
+		}
+		authenticatedUserId = result.Data.Id;
+		myBidsAuctionFilter.BidderId = authenticatedUserId;
+	}
+
 }
