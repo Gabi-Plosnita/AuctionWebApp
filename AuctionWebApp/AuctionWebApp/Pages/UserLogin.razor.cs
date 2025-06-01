12b69b0c72187b5a468c19ee5fd5660f15ee83f0
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-01 16:43:40 +0300

    Create Login User page

---
+++ AuctionWebApp/AuctionWebApp/Pages/UserLogin.razor.cs
@@ -0,0 +1,29 @@
+﻿using AuctionWebApp.Helpers;
+using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Pages;
+
+public partial class UserLogin : ComponentBase
+{
+	[Inject]
+	public IAuthService _authService { get; set; } = default!;
+
+	[Inject]
+	public NavigationManager Navigation { get; set; } = default!;
+
+	private string ReturnUrl { get; set; } = "/";
+
+	protected LoginViewModel LoginModel { get; set; } = new LoginViewModel();
+
+	protected async Task<Result> HandleUserLogin(LoginViewModel model)
+	{
+		var result = await _authService.LoginDriverAsync(model);
+		if (!result.HasErrors)
+		{
+			Navigation.NavigateTo("/user-dashboard");
+		}
+		return result;
+	}
+}
