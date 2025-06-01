073ee3fa904d1cfb85229aef761f253f2c3a65aa
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-01 20:31:57 +0300

    Create DateHelper.cs

---
+++ AuctionWebApp/AuctionWebApp/Helpers/DateHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace AuctionWebApp.Helpers;
+
+public static class DateHelper
+{
+	public static string GetRelativeTime(DateTime dateTime)
+	{
+		var span = DateTime.UtcNow - dateTime.ToUniversalTime();
+		if (span.TotalMinutes < 60)
+			return $"{(int)span.TotalMinutes} min ago";
+		if (span.TotalHours < 24)
+			return $"{(int)span.TotalHours} h ago";
+		if (span.TotalDays < 7)
+			return $"{(int)span.TotalDays} day{(span.TotalDays >= 2 ? "s" : "")} ago";
+		return dateTime.ToString("d MMM yyyy");
+	}
+}
