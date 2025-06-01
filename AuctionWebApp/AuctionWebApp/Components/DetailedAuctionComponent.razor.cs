3cd17db9f05e902f99897d7d701580aec2b3c696
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-01 20:03:34 +0300

    Create BidAuction page and DetailedAuction component

---
+++ AuctionWebApp/AuctionWebApp/Components/DetailedAuctionComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class DetailedAuctionComponent : ComponentBase
+{
+	[Parameter]
+	public required DetailedAuctionViewModel Auction { get; set; }
+
+	private List<string> imageUrls = new List<string>();
+
+	protected override void OnParametersSet()
+	{
+		if (Auction?.Images != null && Auction.Images.Any())
+		{
+			imageUrls = Auction.Images.Select(i => i.ImageUrl).ToList();
+		}
+	}
+}
---
+++ AuctionWebApp/AuctionWebApp/Pages/BidAuction.razor.cs
@@ -0,0 +1,43 @@
+﻿using AuctionWebApp.Helpers;
+using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AuctionWebApp.Pages;
+
+public partial class BidAuction : ComponentBase
+{
+	[Inject]
+	private IAuctionService AuctionService { get; set; } = default!;
+
+	[Inject]
+	private ISnackbar Snackbar { get; set; } = default!;
+
+	[Parameter]
+	public int AuctionId { get; set; }
+
+	private string ReturnUrl { get; set; } = "/user-dashboard";
+
+	private DetailedAuctionViewModel auction;
+
+	private bool isLoading = true;
+
+	protected override async Task OnInitializedAsync()
+	{
+		var auctionResult = await AuctionService.GetDetailedByIdAsync(AuctionId);
+		if (auctionResult.HasErrors)
+		{
+			Snackbar.ShowErrors(auctionResult.Errors);
+			return;
+		}
+		if (auctionResult.Data is null)
+		{
+			Snackbar.ShowError("Auction not found");
+			return;
+		}
+		auction = auctionResult.Data;
+
+		isLoading = false;
+	}
+}
