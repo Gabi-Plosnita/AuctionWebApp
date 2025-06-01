1c7e9c2741f707f6ee3c874c43d8b76edb9d1d3c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-01 14:41:08 +0300

    Create complete auction page

---
+++ AuctionWebApp/AuctionWebApp/Pages/CompleteAuction.razor.cs
@@ -0,0 +1,48 @@
+﻿using AuctionWebApp.Helpers;
+using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AuctionWebApp.Pages;
+
+public partial class CompleteAuction : ComponentBase
+{
+	[Inject]
+	private IAuctionService AuctionService { get; set; } = default!;
+
+	[Inject]
+	private ISnackbar Snackbar { get; set; } = default!;
+
+	[Parameter]
+	public int AuctionId { get; set; }
+
+	private string ReturnUrl { get; set; } = "/driver-dashboard";
+
+	private DetailedAuctionViewModel auction;
+
+	private bool isLoading = true;
+
+	protected override async Task OnInitializedAsync()
+	{
+		var auctionResult = await AuctionService.GetDetailedByIdAsync(AuctionId);
+		if (auctionResult.HasErrors)
+		{
+			Snackbar.ShowErrors(auctionResult.Errors);
+			return;
+		}
+		if (auctionResult.Data is null)
+		{
+			Snackbar.ShowError("Auction not found");
+			return;
+		}
+		auction = auctionResult.Data;
+
+		isLoading = false;
+	}
+
+	private async Task CompleteAuctionAsync()
+	{
+		
+	}
+}
