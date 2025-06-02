ce5536e8eac0f528002301d4415228196ba617ff
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-02 22:12:42 +0300

    Create DriversDashboard page

---
+++ AuctionWebApp/AuctionWebApp/Pages/DriversDashboard.razor.cs
@@ -0,0 +1,26 @@
+﻿using AuctionWebApp.Components;
+using AuctionWebApp.Enums;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Pages;
+
+public partial class DriversDashboard : ComponentBase
+{
+	[Inject]
+	private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
+	private bool isSuperAdmin;
+
+	private bool isLoading = true;
+
+	protected override async Task OnInitializedAsync()
+	{
+		var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+		var user = authState.User;
+
+		isSuperAdmin = user.IsInRole("SuperAdmin");
+		isLoading = false;
+	}
+}
