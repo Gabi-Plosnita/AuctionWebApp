3062f939ed54581aa6042807949edc424447d357
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-04 19:19:49 +0300

    Create not found component

---
+++ AuctionWebApp/AuctionWebApp/Components/NotFoundComponent.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class NotFoundComponent(NavigationManager NavigationManager) : ComponentBase
+{
+	protected void GoHome() => NavigationManager.NavigateTo("/");
+}
