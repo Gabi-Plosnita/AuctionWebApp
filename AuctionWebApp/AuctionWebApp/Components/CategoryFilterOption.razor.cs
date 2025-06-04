094e443729295da8782bbffa7a80c702e448a9b1
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-04 20:56:26 +0300

    Create CategoryOptionComponent

---
+++ AuctionWebApp/AuctionWebApp/Components/CategoryFilterOption.razor.cs
@@ -0,0 +1,10 @@
+﻿using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class CategoryFilterOption : ComponentBase
+{
+	[Parameter]
+	public CategoryViewModel Category { get; set; } = new();
+}
