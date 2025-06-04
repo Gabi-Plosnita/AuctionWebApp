3efba5c39f378c567b5a8503679a5a447cc90a51
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-04 22:40:47 +0300

    Update CategorySlider and CategoryFilterOption components

--- AuctionWebApp/AuctionWebApp/Components/CategoryFilterOption.razor.cs
+++ AuctionWebApp/AuctionWebApp/Components/CategoryFilterOption.razor.cs
@@ -7,4 +7,15 @@ public partial class CategoryFilterOption : ComponentBase
 {
 	[Parameter]
 	public CategoryViewModel Category { get; set; } = new();
+
+	[Parameter]
+	public EventCallback<CategoryViewModel> OnCategoryClicked { get; set; }
+
+	private async Task HandleCardClickAsync()
+	{
+		if (OnCategoryClicked.HasDelegate)
+		{
+			await OnCategoryClicked.InvokeAsync(Category);
+		}
+	}
 }
--- AuctionWebApp/AuctionWebApp/Components/CategorySlider.razor.cs
+++ AuctionWebApp/AuctionWebApp/Components/CategorySlider.razor.cs
@@ -2,85 +2,92 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
-namespace AuctionWebApp.Components;
-
-public partial class CategorySlider : ComponentBase
+namespace AuctionWebApp.Components
 {
-	[Parameter]
-	public List<CategoryViewModel> CategoryViewModels { get; set; } = new List<CategoryViewModel>();
-
-	[Inject]
-	protected IJSRuntime JSRuntime { get; set; } = default!;
-
-	protected ElementReference scrollableDiv;
-	protected bool disableLeftScroll = true;
-	protected bool disableRightScroll = false;
-
-	private const int ItemWidthWithMargin = 128;
-	private const int ScrollAmount = ItemWidthWithMargin * 2;
-
-	protected override async Task OnAfterRenderAsync(bool firstRender)
+	public partial class CategorySlider : ComponentBase
 	{
-		if (firstRender && CategoryViewModels != null && CategoryViewModels.Count > 0)
+		[Parameter]
+		public List<CategoryViewModel> CategoryViewModels { get; set; } = new List<CategoryViewModel>();
+
+		[Inject]
+		protected IJSRuntime JSRuntime { get; set; } = default!;
+
+		protected ElementReference scrollableDiv;
+		protected bool disableLeftScroll = true;
+		protected bool disableRightScroll = false;
+
+		private const int ItemWidthWithMargin = 128;
+		private const int ScrollAmount = ItemWidthWithMargin * 2;
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
 		{
-			await Task.Delay(50);
+			if (firstRender && CategoryViewModels != null && CategoryViewModels.Count > 0)
+			{
+				await Task.Delay(50);
+				await UpdateScrollButtonStatesAsync();
+			}
+			else if (firstRender)
+			{
+				disableLeftScroll = true;
+				disableRightScroll = true;
+				StateHasChanged();
+			}
+		}
+
+		protected async Task ScrollLeft()
+		{
+			await JSRuntime.InvokeVoidAsync("categorySliderFunctions.scrollElementHorizontal", scrollableDiv, -ScrollAmount);
+			await Task.Delay(350);
 			await UpdateScrollButtonStatesAsync();
 		}
-		else if (firstRender)
+
+		protected async Task ScrollRight()
 		{
-			disableLeftScroll = true;
-			disableRightScroll = true;
+			await JSRuntime.InvokeVoidAsync("categorySliderFunctions.scrollElementHorizontal", scrollableDiv, ScrollAmount);
+			await Task.Delay(350);
+			await UpdateScrollButtonStatesAsync();
+		}
+
+		protected async Task HandleScrollAsync()
+		{
+			await UpdateScrollButtonStatesAsync();
+		}
+
+		private async Task UpdateScrollButtonStatesAsync()
+		{
+			if (scrollableDiv.Context == null)
+			{
+				return;
+			}
+
+			var scrollProps = await JSRuntime.InvokeAsync<ScrollProperties>("categorySliderFunctions.getElementScrollProperties", scrollableDiv);
+
+			if (scrollProps == null) return;
+
+			if (scrollProps.ScrollWidth <= scrollProps.ClientWidth)
+			{
+				disableLeftScroll = true;
+				disableRightScroll = true;
+			}
+			else
+			{
+				disableLeftScroll = scrollProps.ScrollLeft <= 0;
+				disableRightScroll = scrollProps.ScrollLeft + scrollProps.ClientWidth >= scrollProps.ScrollWidth - 1;
+			}
 			StateHasChanged();
 		}
-	}
 
-	protected async Task ScrollLeft()
-	{
-		await JSRuntime.InvokeVoidAsync("categorySliderFunctions.scrollElementHorizontal", scrollableDiv, -ScrollAmount);
-		await Task.Delay(350); 
-		await UpdateScrollButtonStatesAsync();
-	}
-
-	protected async Task ScrollRight()
-	{
-		await JSRuntime.InvokeVoidAsync("categorySliderFunctions.scrollElementHorizontal", scrollableDiv, ScrollAmount);
-		await Task.Delay(350);
-		await UpdateScrollButtonStatesAsync();
-	}
-
-	protected async Task HandleScrollAsync()
-	{
-		await UpdateScrollButtonStatesAsync();
-	}
-
-	private async Task UpdateScrollButtonStatesAsync()
-	{
-		if (scrollableDiv.Context == null) 
+		protected async Task HandleCategorySelectionAsync(CategoryViewModel selectedCategory)
 		{
-			return;
+			System.Console.WriteLine($"Selected category: {selectedCategory.Name}, ID: {selectedCategory.Id}");
+			await Task.CompletedTask;
 		}
 
-		var scrollProps = await JSRuntime.InvokeAsync<ScrollProperties>("categorySliderFunctions.getElementScrollProperties", scrollableDiv);
-
-		if (scrollProps == null) return;
-
-		if (scrollProps.ScrollWidth <= scrollProps.ClientWidth)
+		private class ScrollProperties
 		{
-			disableLeftScroll = true;
-			disableRightScroll = true;
+			public double ScrollLeft { get; set; }
+			public double ScrollWidth { get; set; }
+			public double ClientWidth { get; set; }
 		}
-		else
-		{
-			disableLeftScroll = scrollProps.ScrollLeft <= 0;
-			disableRightScroll = scrollProps.ScrollLeft + scrollProps.ClientWidth >= scrollProps.ScrollWidth - 1;
-		}
-		StateHasChanged(); 
-	}
-
-	private class ScrollProperties
-	{
-		public double ScrollLeft { get; set; }
-		public double ScrollWidth { get; set; }
-		public double ClientWidth { get; set; }
 	}
 }
