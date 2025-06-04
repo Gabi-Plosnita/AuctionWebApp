0249f5c901db3a4d91e6a47607f9d854807a7f27
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-04 21:34:22 +0300

    Create CategorySlider component

---
+++ AuctionWebApp/AuctionWebApp/Components/CategorySlider.razor.cs
@@ -0,0 +1,86 @@
+﻿using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace AuctionWebApp.Components;
+
+public partial class CategorySlider : ComponentBase
+{
+	[Parameter]
+	public List<CategoryViewModel> CategoryViewModels { get; set; } = new List<CategoryViewModel>();
+
+	[Inject]
+	protected IJSRuntime JSRuntime { get; set; } = default!;
+
+	protected ElementReference scrollableDiv;
+	protected bool disableLeftScroll = true;
+	protected bool disableRightScroll = false;
+
+	private const int ItemWidthWithMargin = 128;
+	private const int ScrollAmount = ItemWidthWithMargin * 2;
+
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		if (firstRender && CategoryViewModels != null && CategoryViewModels.Count > 0)
+		{
+			await Task.Delay(50);
+			await UpdateScrollButtonStatesAsync();
+		}
+		else if (firstRender)
+		{
+			disableLeftScroll = true;
+			disableRightScroll = true;
+			StateHasChanged();
+		}
+	}
+
+	protected async Task ScrollLeft()
+	{
+		await JSRuntime.InvokeVoidAsync("categorySliderFunctions.scrollElementHorizontal", scrollableDiv, -ScrollAmount);
+		await Task.Delay(350); 
+		await UpdateScrollButtonStatesAsync();
+	}
+
+	protected async Task ScrollRight()
+	{
+		await JSRuntime.InvokeVoidAsync("categorySliderFunctions.scrollElementHorizontal", scrollableDiv, ScrollAmount);
+		await Task.Delay(350);
+		await UpdateScrollButtonStatesAsync();
+	}
+
+	protected async Task HandleScrollAsync()
+	{
+		await UpdateScrollButtonStatesAsync();
+	}
+
+	private async Task UpdateScrollButtonStatesAsync()
+	{
+		if (scrollableDiv.Context == null) 
+		{
+			return;
+		}
+
+		var scrollProps = await JSRuntime.InvokeAsync<ScrollProperties>("categorySliderFunctions.getElementScrollProperties", scrollableDiv);
+
+		if (scrollProps == null) return;
+
+		if (scrollProps.ScrollWidth <= scrollProps.ClientWidth)
+		{
+			disableLeftScroll = true;
+			disableRightScroll = true;
+		}
+		else
+		{
+			disableLeftScroll = scrollProps.ScrollLeft <= 0;
+			disableRightScroll = scrollProps.ScrollLeft + scrollProps.ClientWidth >= scrollProps.ScrollWidth - 1;
+		}
+		StateHasChanged(); 
+	}
+
+	private class ScrollProperties
+	{
+		public double ScrollLeft { get; set; }
+		public double ScrollWidth { get; set; }
+		public double ClientWidth { get; set; }
+	}
+}
---
+++ AuctionWebApp/AuctionWebApp/wwwroot/js/categorySlider.js
@@ -0,0 +1,17 @@
+﻿window.categorySliderFunctions = {
+    scrollElementHorizontal: function (element, amount) {
+        if (element) {
+            element.scrollBy({ left: amount, behavior: 'smooth' });
+        }
+    },
+    getElementScrollProperties: function (element) {
+        if (element) {
+            return {
+                scrollLeft: element.scrollLeft,
+                scrollWidth: element.scrollWidth,
+                clientWidth: element.clientWidth
+            };
+        }
+        return null; 
+    }
+};
