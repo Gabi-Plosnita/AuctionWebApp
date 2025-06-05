953ed56f23d6fc9b92e71895e0356a37d903dfe4
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-05 20:21:09 +0300

    Create PreviewAuctionComponent

---
+++ AuctionWebApp/AuctionWebApp/Components/PreviewAuctionCardComponent.razor.cs
@@ -0,0 +1,20 @@
+﻿using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class PreviewAuctionCardComponent(NavigationManager NavigationManager) : ComponentBase
+{
+	[Parameter]
+	public PreviewAuctionViewModel Auction { get; set; } = new PreviewAuctionViewModel();
+
+	[Parameter]
+	public string NavigationUrl { get; set; } = string.Empty;
+
+	private string FirstImageUrl => Auction.Images?.FirstOrDefault()?.ImageUrl ?? "placeholder.jpg";
+
+	private void HandleClick()
+	{
+		NavigationManager.NavigateTo(NavigationUrl);
+	}
+}
