c519264a4433405e190912f5a017ea38be0e8a8c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-06 14:30:01 +0300

    Update AuctionProfile.cs

--- AuctionWebApp/AuctionWebApp/Mappings/AuctionProfile.cs
+++ AuctionWebApp/AuctionWebApp/Mappings/AuctionProfile.cs
@@ -9,7 +9,16 @@ public class AuctionProfile : Profile
 	public AuctionProfile()
 	{
 		CreateMap<AuctionFilterViewModel, AuctionFilterRequest>();
-		CreateMap<CreateAuctionViewModel, CreateAuctionRequest>();
+		CreateMap<CreateAuctionViewModel, CreateAuctionRequest>()
+			.ForMember(
+				dest => dest.Images,
+				opt => opt.MapFrom(src =>
+					src.Images
+					   .Where(kv => kv.Value != null)
+					   .Select(kv => kv.Value!)
+					   .ToList()
+				)
+			);
 		CreateMap<DetailedAuctionResponse, DetailedAuctionViewModel>();
 		CreateMap<PreviewAuctionResponse, PreviewAuctionViewModel>();
 		CreateMap<UpdateAuctionViewModel, UpdateAuctionRequest>();
