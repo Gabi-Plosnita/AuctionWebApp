d35f15cc14d325869de9785940c1936ec81bae92
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-06 11:31:57 +0300

    Create AuctionDetails page

---
+++ AuctionWebApp/AuctionWebApp/Pages/User/AuctionDetails.razor.cs
@@ -0,0 +1,39 @@
+﻿using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AuctionWebApp.Pages;
+
+public partial class AuctionDetails(IAuctionService AuctionService) : ComponentBase
+{
+	[Parameter]
+	public int AuctionId { get; set; }
+
+	private DetailedAuctionViewModel auction;
+
+	private bool isLoading = true;
+
+	private bool showErrorComponent = false;
+
+	protected override async Task OnInitializedAsync()
+	{
+		await InitializeAuctionAsync();
+		if (showErrorComponent || !isLoading)
+			return;
+
+		isLoading = false;
+	}
+
+	private async Task InitializeAuctionAsync()
+	{
+		var auctionResult = await AuctionService.GetDetailedByIdAsync(AuctionId);
+		if (auctionResult.HasErrors || auctionResult.Data is null)
+		{
+			showErrorComponent = true;
+			isLoading = false;
+			return;
+		}
+		auction = auctionResult.Data;
+	}
+}
