8c8e0243de97f0cbfae757e3f434eb7a50f3198e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-06 12:58:35 +0300

    Update CreateAuction.razor.cs

--- AuctionWebApp/AuctionWebApp/Pages/User/CreateAuction.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/User/CreateAuction.razor.cs
@@ -2,13 +2,17 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.JSInterop;
-using System.ComponentModel.DataAnnotations;
 
 namespace AuctionWebApp.Pages;
 
 public partial class CreateAuction(IJSRuntime JSRuntime) : ComponentBase
 {
-	private readonly CreateAuctionViewModel _model = new() { EndTime = DateTime.Now.AddDays(7) };
+	private readonly CreateAuctionViewModel _model = new() 
+	{ 
+		EndTime = DateTime.Now.AddDays(7), 
+		StartingPrice = 1,
+		MinBidIncrement = 1,
+	};
 	private readonly Dictionary<int, string> _imagePreviews = new();
 	private int _currentImageIndex;
 
@@ -43,43 +47,5 @@ public partial class CreateAuction(IJSRuntime JSRuntime) : ComponentBase
 	{
 		_model.Images.Remove(index);
 		_imagePreviews.Remove(index);
-	}
-
-	public class CreateAuctionViewModel
-	{
-		[Required(ErrorMessage = "Title is required")]
-		[StringLength(200, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 200 characters")]
-		public string Title { get; set; } = string.Empty;
-
-		[Required(ErrorMessage = "Description is required")]
-		[StringLength(2000, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 2000 characters")]
-		public string Description { get; set; } = string.Empty;
-
-		[EnsureOneImage(ErrorMessage = "You must upload at least one image.")]
-		public Dictionary<int, IBrowserFile> Images { get; set; } = new();
-
-		[Range(1, double.MaxValue, ErrorMessage = "StartingPrice must be at least 1")]
-		public decimal StartingPrice { get; set; } = 1.00m;
-
-		[Range(1, double.MaxValue, ErrorMessage = "MinBidIncrement must be at least 1")]
-		public decimal MinBidIncrement { get; set; } = 1.00m;
-
-		public DateTime EndTime { get; set; }
-
-		[Range(1, int.MaxValue, ErrorMessage = "CategoryId must be a positive integer")]
-		public int CategoryId { get; set; }
-	}
-
-	public class EnsureOneImageAttribute : ValidationAttribute
-	{
-		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-		{
-			var images = value as Dictionary<int, IBrowserFile>;
-			if (images == null || images.Count == 0)
-			{
-				return new ValidationResult(ErrorMessage);
-			}
-			return ValidationResult.Success;
-		}
 	}
 }
