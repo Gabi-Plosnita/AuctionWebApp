336809e4003b5a439fa295287f93634ae7032a50
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-06 18:29:16 +0300

    Use the user settings component in the home page

--- AuctionWebApp/AuctionWebApp/Pages/User/UserHome.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/User/UserHome.razor.cs
@@ -1,12 +1,48 @@
-﻿using AuctionWebApp.Components;
-using AuctionWebApp.Enums;
-using AuctionWebApp.Services;
+﻿using AuctionWebApp.Services;
 using AuctionWebApp.ViewModels;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace AuctionWebApp.Pages;
 
-public partial class UserHome() : ComponentBase
+public partial class UserHome(IUserService UserService,
+							  IAuthService AuthService,
+							  ISnackbar Snackbar) : ComponentBase
 {
-	// add isLoading
+	private UserViewModel userViewModel { get; set; } = new();
+
+	bool isLoading = true;
+
+	bool showErrorComponent = false;
+
+	protected override async Task OnInitializedAsync()
+	{
+		await AuthenticateUserAsync();
+
+		if (showErrorComponent || !isLoading)
+			return;
+
+		isLoading = false;
+	}
+
+	private async Task AuthenticateUserAsync()
+	{
+		var authenticatedUserResult = await AuthService.GetAuthenticatedUserAsync();
+		if(authenticatedUserResult.HasErrors || authenticatedUserResult.Data == null)
+		{
+			showErrorComponent = true;
+			isLoading = false;
+			return;
+		}
+
+		var userViewModelResult = await UserService.GetByIdAsync(authenticatedUserResult.Data.Id);
+		if(userViewModelResult.HasErrors || userViewModelResult.Data == null)
+		{
+			showErrorComponent = true;
+			isLoading = false; 
+			return;
+		}
+
+		userViewModel = userViewModelResult.Data;
+	}
 }
