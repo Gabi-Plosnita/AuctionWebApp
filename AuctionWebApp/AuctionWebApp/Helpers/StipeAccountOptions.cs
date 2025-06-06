59430629541416b657823bd06202ea67a9274d31
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-07 00:14:14 +0300

    Create StipeAccountOptions.cs

---
+++ AuctionWebApp/AuctionWebApp/Helpers/StipeAccountOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace AuctionWebApp.Helpers;
+
+public static class StripeAccountOptions
+{
+	public static readonly List<string> Countries = new()
+		{
+			"US", "CA", "GB", "FR", "DE", "IT", "AU", "NL", "SE", "JP", "RO"
+		};
+
+	public static readonly List<string> Currencies = new()
+		{
+			"USD", "CAD", "GBP", "EUR", "AUD", "JPY", "RON"
+		};
+}
