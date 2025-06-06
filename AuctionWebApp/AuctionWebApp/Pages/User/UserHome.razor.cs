8d2b1c85847df56563a92fdbd87c5d60c8b4eedc
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-06 18:57:02 +0300

    Create UserSettings page and udpate UserSettings component

--- AuctionWebApp/AuctionWebApp/Pages/User/UserHome.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/User/UserHome.razor.cs
@@ -1,48 +1,8 @@
-﻿using AuctionWebApp.Services;
-using AuctionWebApp.ViewModels;
-using Microsoft.AspNetCore.Components;
-using MudBlazor;
+﻿using Microsoft.AspNetCore.Components;
 
 namespace AuctionWebApp.Pages;
 
-public partial class UserHome(IUserService UserService,
-							  IAuthService AuthService,
-							  ISnackbar Snackbar) : ComponentBase
+public partial class UserHome() : ComponentBase
 {
-	private UserViewModel userViewModel { get; set; } = new();
-
-	bool isLoading = true;
-
-	bool showErrorComponent = false;
-
-	protected override async Task OnInitializedAsync()
-	{
-		await AuthenticateUserAsync();
-
-		if (showErrorComponent || !isLoading)
-			return;
-
-		isLoading = false;
-	}
-
-	private async Task AuthenticateUserAsync()
-	{
-		var authenticatedUserResult = await AuthService.GetAuthenticatedUserAsync();
-		if(authenticatedUserResult.HasErrors || authenticatedUserResult.Data == null)
-		{
-			showErrorComponent = true;
-			isLoading = false;
-			return;
-		}
-
-		var userViewModelResult = await UserService.GetByIdAsync(authenticatedUserResult.Data.Id);
-		if(userViewModelResult.HasErrors || userViewModelResult.Data == null)
-		{
-			showErrorComponent = true;
-			isLoading = false; 
-			return;
-		}
-
-		userViewModel = userViewModelResult.Data;
-	}
+	
 }
---
+++ AuctionWebApp/AuctionWebApp/Pages/User/UserSettings.razor.cs
@@ -0,0 +1,73 @@
+﻿using AuctionWebApp.Helpers;
+using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AuctionWebApp.Pages;
+
+public partial class UserSettings(IUserService UserService,
+							      IAuthService AuthService,
+							      ISnackbar Snackbar) : ComponentBase
+{
+	private MudForm? _editForm;
+	private UserViewModel userViewModel { get; set; } = new();
+	private UpdateUserViewModel updateUserViewModel { get; set; } = new();
+
+	private bool isLoading = true;
+
+	private bool showErrorComponent = false;
+
+	protected override async Task OnInitializedAsync()
+	{
+		await AuthenticateUserAsync();
+		if (showErrorComponent || !isLoading)
+			return;
+
+		isLoading = false;
+	}
+
+	private async Task AuthenticateUserAsync()
+	{
+		var authenticatedUserResult = await AuthService.GetAuthenticatedUserAsync();
+		if (authenticatedUserResult.HasErrors || authenticatedUserResult.Data == null)
+		{
+			showErrorComponent = true;
+			isLoading = false;
+			return;
+		}
+
+		var userViewModelResult = await UserService.GetByIdAsync(authenticatedUserResult.Data.Id);
+		if (userViewModelResult.HasErrors || userViewModelResult.Data == null)
+		{
+			showErrorComponent = true;
+			isLoading = false;
+			return;
+		}
+
+		userViewModel = userViewModelResult.Data;
+		SetUpdateUserViewModel();
+	}
+
+	private void SetUpdateUserViewModel()
+	{
+		updateUserViewModel.FirstName = userViewModel.FirstName;
+		updateUserViewModel.LastName = userViewModel.LastName;
+		updateUserViewModel.Address = userViewModel.Address;
+	}
+
+	private async Task UpdateAsync()
+	{
+		if (_editForm is null)
+			return;
+
+		await _editForm.Validate();
+		if (_editForm.IsValid)
+		{
+			userViewModel.FirstName = updateUserViewModel.FirstName;
+			userViewModel.LastName = updateUserViewModel.LastName;
+			userViewModel.Address = updateUserViewModel.Address;
+			Snackbar.ShowSuccess("Profile updated successfully!");
+		}
+	}
+}
