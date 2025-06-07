61a743f6721dae6db781a2fe896b5506129adadc
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-07 15:17:57 +0300

    Create customer settings page

---
+++ AuctionWebApp/AuctionWebApp/Pages/User/CustomerSettings.razor.cs
@@ -0,0 +1,30 @@
+﻿using AuctionWebApp.Services;
+using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Pages;
+
+public partial class CustomerSettings(IUserService UserService) : ComponentBase
+{
+	private StripePaymentMethodViewModel? paymentMethod;
+	private bool isLoading = true;
+
+	protected override async Task OnInitializedAsync()
+	{
+		await InitializePaymentMethodAsync();
+	}
+
+	private async Task InitializePaymentMethodAsync()
+	{
+		isLoading = true;
+		var result = await UserService.GetStripePaymentMethodAsync();
+		if (result.HasErrors || result.Data == null)
+		{
+			isLoading = false;
+			return;
+		}
+
+		paymentMethod = result.Data;
+		isLoading = false;
+	}
+}
