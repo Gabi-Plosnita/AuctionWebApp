e73a300b1c1c8fbe40f77636b517f1a1bb72cc19
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-07 15:17:49 +0300

    Create CardComponent

---
+++ AuctionWebApp/AuctionWebApp/Components/CardInformationComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using AuctionWebApp.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AuctionWebApp.Components;
+
+public partial class CardInformationComponent : ComponentBase
+{
+	[Parameter]
+	public StripePaymentMethodViewModel? PaymentMethod { get; set; }
+}
