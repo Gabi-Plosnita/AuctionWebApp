e77f41f39e7b4b7164ab7cc8401149b0347aa4c2
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-07 19:23:28 +0300

    Update CustomerSettings page

--- AuctionWebApp/AuctionWebApp/Pages/User/CustomerSettings.razor.cs
+++ AuctionWebApp/AuctionWebApp/Pages/User/CustomerSettings.razor.cs
@@ -1,4 +1,5 @@
-﻿using AuctionWebApp.Services;
+﻿using AuctionWebApp.HttpClients;
+using AuctionWebApp.Services;
 using AuctionWebApp.ViewModels;
 using Microsoft.AspNetCore.Components;
 
@@ -7,7 +8,13 @@ namespace AuctionWebApp.Pages;
 public partial class CustomerSettings(IUserService UserService) : ComponentBase
 {
 	private StripePaymentMethodViewModel? paymentMethod;
+	private bool isCreateMode => paymentMethod == null;
+	private string paymentMethodTabName => isCreateMode ? "Add Payment Method" : "Update Payment Method";
+
+	private int activeTabIndex = 0;
+
 	private bool isLoading = true;
+
 
 	protected override async Task OnInitializedAsync()
 	{
@@ -26,5 +33,13 @@ public partial class CustomerSettings(IUserService UserService) : ComponentBase
 
 		paymentMethod = result.Data;
 		isLoading = false;
+	}
+
+	private async Task HandleTabChange(int index)
+	{
+		if (index == 0) 
+		{
+			await InitializePaymentMethodAsync();
+		}
 	}
 }
