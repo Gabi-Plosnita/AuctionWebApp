a3390de93adbf4f49dc03bc9d6150dd05594833f
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-06-07 19:06:05 +0300

    Create StripePaymentMethodResult.cs

---
+++ AuctionWebApp/AuctionWebApp/Helpers/StripePaymentMethodResult.cs
@@ -0,0 +1,7 @@
+﻿namespace AuctionWebApp.Helpers;
+
+public class StripePaymentMethodResult
+{
+	public string PaymentMethodId { get; set; } = string.Empty;
+	public string Error { get; set; } = string.Empty;
+}
